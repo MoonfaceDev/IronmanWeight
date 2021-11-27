815e24cf1d9a1e7de00240ff8308aadc3627fb07
elaic <elaicorem1@gmail.com>
2021-11-27 19:56:34 +0200

    Improved response format

--- src/main/java/BotListenerAdapter.java
+++ src/main/java/BotListenerAdapter.java
@@ -1,12 +1,14 @@
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.MessageBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.sql.Timestamp;
 import java.text.DecimalFormat;
+import java.time.Instant;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 
 public class BotListenerAdapter extends ListenerAdapter {
 
@@ -29,10 +31,6 @@ public class BotListenerAdapter extends ListenerAdapter {
             double weight = player.getTotalWeight();
             String response = ":pick: "+playerName+" | "+profileName+""+
                     "\n**Total Weight:** "+weightFormat.format(weight)+"\n"+
-                    "\n**Catacombs:** "+weightFormat.format(player.catacombs.getWeight())+
-                    "\n**Mithril Powder:** "+weightFormat.format(player.mithrilPowder.getWeight())+
-                    "\n**Gemstone Powder:** "+weightFormat.format(player.gemstonePowder.getWeight())+
-                    "\n**Minion Slots:** "+weightFormat.format(player.minionSlots.getWeight())+
                     "\n**Skills:** "+weightFormat.format(player.getSkillsWeight())+
                     "\n - Taming: "+weightFormat.format(player.taming.getWeight())+
                     "\n - Farming: "+weightFormat.format(player.farming.getWeight())+
@@ -42,17 +40,23 @@ public class BotListenerAdapter extends ListenerAdapter {
                     "\n - Fishing: "+weightFormat.format(player.fishing.getWeight())+
                     "\n - Enchanting: "+weightFormat.format(player.enchanting.getWeight())+
                     "\n - Alchemy: "+weightFormat.format(player.alchemy.getWeight())+
+                    "\n**Catacombs:** "+weightFormat.format(player.catacombs.getWeight())+
                     "\n**Slayers:** "+weightFormat.format(player.getSlayersWeight())+
                     "\n - Zombie: "+weightFormat.format(player.zombie.getWeight())+
                     "\n - Spider: "+weightFormat.format(player.spider.getWeight())+
                     "\n - Wolf: "+weightFormat.format(player.wolf.getWeight())+
                     "\n - Enderman: "+weightFormat.format(player.enderman.getWeight())+
-                    "\n**Pet Score:** "+weightFormat.format(player.petScore.getWeight())+
                     "\n**Items:** "+weightFormat.format(player.getItemsWeight()+player.getPetsWeight())+
-                    "\n**Talismans:** "+weightFormat.format(player.missingTalismans.getWeight()+player.missingTalismanUpgrades.getWeight());
+                    "\n**Talismans:** "+weightFormat.format(player.missingTalismans.getWeight()+player.missingTalismanUpgrades.getWeight()+
+                    "\n**Mithril Powder:** "+weightFormat.format(player.mithrilPowder.getWeight())+
+                    "\n**Gemstone Powder:** "+weightFormat.format(player.gemstonePowder.getWeight())+
+                    "\n**Pet Score:** "+weightFormat.format(player.petScore.getWeight())+
+                    "\n**Minion Slots:** "+weightFormat.format(player.minionSlots.getWeight()));
             event.getChannel().sendMessageEmbeds(new EmbedBuilder()
                             .setDescription(response)
                             .setColor(Color.decode("#304ffe"))
+                            .setFooter("Ironman Weight • Created By nomface")
+                            .setTimestamp(Instant.now())
                             .build()
             ).queue();
         }
